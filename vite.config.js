b7e75178c4e7845a5de638fcd0683ba00d70f3aa
Talmage Sharp <talmagesharp321@gmail.com>
2026-04-10 18:51:41 -0600

    update config to not use local pem files

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from "node:fs";
 import path from "node:path";
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        host: true,
-        port: 5174,
-        https: {
-            key: fs.readFileSync(path.resolve(__dirname, "certs/dev-key.pem")),
-            cert: fs.readFileSync(path.resolve(__dirname, "certs/dev-cert.pem")),
+export default defineConfig(({ command }) => {
+    const isDev = command === 'serve';
+
+    return {
+        plugins: [react()],
+        server: {
+            host: true,
+            port: 5174,
+            https: isDev
+                ? {
+                    key: fs.readFileSync(path.resolve(__dirname, "certs/dev-key.pem")),
+                    cert: fs.readFileSync(path.resolve(__dirname, "certs/dev-cert.pem")),
+                }
+                : false,
         },
-    },
+    };
 });
